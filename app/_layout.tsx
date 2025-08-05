9d4ffdf0dd0a63b6456d943b1bc6dee3f20d62de
John Ayodeji <johnayodeji2008@gmail.com>
2025-08-05 17:14:48 +0100

    prebuild

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,14 +2,18 @@ import { Stack } from 'expo-router'
 import { useEffect } from 'react'
 import * as QuickActions from 'expo-quick-actions'
 import { Platform } from 'react-native'
+import { useQuickActionRouting } from 'expo-quick-actions/router'
 
 const Layout = () => {
+  useQuickActionRouting()
   useEffect(() => {
+    // useQuickActionRouting()
     QuickActions.setItems([
       {
         title: "Add a plant",
         icon: Platform.OS === 'ios' ? 'symbol:leaf' : 'leaf',
-        id: '0'
+        id: '0',
+        params: { href: '/new' }
       }
     ])
   }, [])
